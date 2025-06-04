0cfb57ca228bf54c3e5d33d40ea2ce69eb821c61
Carl van Vliet <carl@vvliet.de>
2025-06-04 13:42:50 +0200

    mysql utm vm connection

--- JDBC/src/main/java/fhwedel/JDBC/Hello.java
+++ JDBC/src/main/java/fhwedel/JDBC/Hello.java
@@ -1,7 +1,14 @@
 package fhwedel.JDBC;
 
+import java.sql.*;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, JDBC world!");
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mariadb://192.168.64.2:3306/firma",
+                "user", "user"
+        );
+
+
     }
 }
